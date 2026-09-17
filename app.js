b3ea6c230bfbb2d61728910f0f7fdc6f63de32a8
srnimue <45224188+srnimue@users.noreply.github.com>
2026-09-18 07:40:10 +0900

    Update app.js

--- app.js
+++ app.js
@@ -346,9 +346,9 @@ function stopManualMode() {
 function finishStamp(point) {
     if (!dragStart) return;
     const x = Math.min(dragStart.x, point.x);
-    const y = Math.min(dragStart.y, point.y)-15;
+    const y = Math.min(dragStart.y, point.y)-20;
     const w = Math.abs(point.x - dragStart.x);
-    const h = Math.abs(point.y - dragStart.y)+30;
+    const h = Math.abs(point.y - dragStart.y)+10;
     selection.hidden = true;
 
     if (w < 4 || h < 4) {
